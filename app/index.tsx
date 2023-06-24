e1c31564eac1c0681b2bbb2bc1ba7eb08e93ac4d
shelby.rothman <shelby.rothman@aderant.com>
2023-06-23 17:12:34 -0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import QuestionListItem from '../src/components/QuestionListItem';
+import questions from '../data/questions.json';
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={questions.items}
+        renderItem={({ item }) => <QuestionListItem question={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
---
+++ babel.config.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: [ "babel-preset-expo" ],
+        plugins: [
+            "@babel/plugin-proposal-export-namespace-from",
+            "react-native-reanimated/plugin",
+            require.resolve("expo-router/babel"),
+        ],
+    };
+};
